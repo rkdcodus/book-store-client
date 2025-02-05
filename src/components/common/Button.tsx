e186a6b976be62b264bcf9e792d7efc61b233820
rkdcodus <rcd7275@naver.com>
2025-02-05 15:41:59 +0900

    feat: 도서 카테고리에 따라 params 조정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -10,9 +10,9 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   isLoading?: boolean;
 }
 
-function Button({ children, size, scheme, disabled, isLoading }: Props) {
+function Button({ children, size, scheme, disabled, isLoading, ...props }: Props) {
   return (
-    <ButtonStyle size={size} scheme={scheme} disabled={disabled} isLoading={isLoading}>
+    <ButtonStyle size={size} scheme={scheme} disabled={disabled} isLoading={isLoading} {...props}>
       {children}
     </ButtonStyle>
   );
--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -1,4 +1,5 @@
 export interface Category {
   category_id: number | null;
   category: string;
+  isActive?: boolean;
 }
