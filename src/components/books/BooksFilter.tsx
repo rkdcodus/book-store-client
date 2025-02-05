e01976b313f6d091ee2bb60293b2642e272fda73
rkdcodus <rcd7275@naver.com>
2025-02-05 15:13:15 +0900

    feat: BookEmpty, BooksFilter 스타일 구현

--- src/components/books/BooksFilter.tsx
+++ src/components/books/BooksFilter.tsx
@@ -1,9 +1,36 @@
 import { styled } from "styled-components";
+import { useCategory } from "../../hooks/useCategory";
+import Button from "../common/Button";
 
 function BooksFilter() {
-  return <BooksFilterStyle></BooksFilterStyle>;
+  const { category } = useCategory();
+
+  return (
+    <BooksFilterStyle>
+      <div className='category'>
+        {category.map((item) => (
+          <Button size='medium' scheme='normal' key={item.category_id}>
+            {item.category}
+          </Button>
+        ))}
+      </div>
+      <div className='new'>
+        <Button size='medium' scheme='normal'>
+          신간
+        </Button>
+      </div>
+    </BooksFilterStyle>
+  );
 }
 
-const BooksFilterStyle = styled.div``;
+const BooksFilterStyle = styled.div`
+  display: flex;
+  gap: 24px;
+
+  .category {
+    display: flex;
+    gap: 8px;
+  }
+`;
 
 export default BooksFilter;
