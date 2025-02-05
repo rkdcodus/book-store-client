e7ca44cb8cc6f311697ad41ee75c5152d81b8b77
rkdcodus <rcd7275@naver.com>
2025-02-05 13:47:11 +0900

    feat: 로그인 시 헤더 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,9 +3,12 @@ import logo from "../../assets/images/logo.png";
 import { FaRegUser, FaSignInAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useCategory } from "../../hooks/useCategory";
+import { useAuthStore } from "../../store/authStore";
 
 function Header() {
   const { category } = useCategory();
+
+  const { isLoggedIn, storeLogout } = useAuthStore();
 
   return (
     <HeaderStyle>
@@ -26,20 +29,35 @@ function Header() {
         </ul>
       </nav>
       <nav className='auth'>
-        <ul>
-          <li>
-            <Link to='/login'>
-              <FaSignInAlt />
-              로그인
-            </Link>
-          </li>
-          <li>
-            <Link to='/signup'>
-              <FaRegUser />
-              회원가입
-            </Link>
-          </li>
-        </ul>
+        {isLoggedIn && (
+          <ul>
+            <li>
+              <Link to='/cart'>장바구니</Link>
+            </li>
+            <li>
+              <Link to='/orderlist'>주문 내역</Link>
+            </li>
+            <li>
+              <button onClick={storeLogout}>로그아웃</button>
+            </li>
+          </ul>
+        )}
+        {!isLoggedIn && (
+          <ul>
+            <li>
+              <Link to='/login'>
+                <FaSignInAlt />
+                로그인
+              </Link>
+            </li>
+            <li>
+              <Link to='/signup'>
+                <FaRegUser />
+                회원가입
+              </Link>
+            </li>
+          </ul>
+        )}
       </nav>
     </HeaderStyle>
   );
@@ -87,7 +105,8 @@ const HeaderStyle = styled.header`
       gap: 32px;
 
       li {
-        a {
+        a,
+        button {
           font-size: 1rem;
           font-weight: 600;
           text-decoration: none;
@@ -95,6 +114,9 @@ const HeaderStyle = styled.header`
           display: flex;
           align-item: center;
           line-height: 1;
+          background: none;
+          border: 0;
+          cursor: pointer;
 
           svg {
             margin-right: 6px;
