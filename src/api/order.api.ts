93af631eb1c2b4e7b7304b9faec2e5e4611da707
rkdcodus <rcd7275@naver.com>
2025-02-10 23:22:18 +0900

    refactor: api requestHandler로 중복 코드 제거

--- src/api/order.api.ts
+++ src/api/order.api.ts
@@ -1,12 +1,14 @@
 import { Order, OrderSheet } from "../models/order.model";
-import { httpClient } from "./http";
+import { requestHandler } from "./http";
 
 export const order = async (orderData: OrderSheet) => {
-  const response = await httpClient.post("/order", orderData);
-  return response.data;
+  // const response = await httpClient.post("/order", orderData);
+  // return response.data;
+  return await requestHandler<OrderSheet>("post", "/order", orderData);
 };
 
 export const fetchOrder = async () => {
-  const response = await httpClient.get<Order>("/order");
-  return response.data;
+  // const response = await httpClient.get<Order>("/order");
+  // return response.data;
+  return await requestHandler<Order>("get", "/order");
 };
